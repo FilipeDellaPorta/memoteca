dd334cb4bc46f15cc3f1d646c308d0d06818e861
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-03-11 12:33:41 -0300

    feat: :sparkles: trabalhando com ngFor para renderizar a lista de cards no mural

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PensamentoCardComponent } from '../pensamento-card/pensamento-card.component';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-listar-pensamentos',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, PensamentoCardComponent, CommonModule, NgFor],
   templateUrl: './listar-pensamentos.component.html',
-  styleUrl: './listar-pensamentos.component.css'
+  styleUrl: './listar-pensamentos.component.css',
 })
 export class ListarPensamentosComponent {
-
+  listaPensamentos = [];
 }
