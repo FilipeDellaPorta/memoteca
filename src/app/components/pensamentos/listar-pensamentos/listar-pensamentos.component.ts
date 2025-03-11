ad558bb005a9ff5fcb9aa6f7d49368af1a0ad5c3
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-03-11 12:35:23 -0300

    fix: :bug: ngFor está dentro de CommonModule não precisando importar separadamente

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PensamentoCardComponent } from '../pensamento-card/pensamento-card.component';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listar-pensamentos',
   standalone: true,
-  imports: [RouterLink, PensamentoCardComponent, CommonModule, NgFor],
+  imports: [RouterLink, PensamentoCardComponent, CommonModule],
   templateUrl: './listar-pensamentos.component.html',
   styleUrl: './listar-pensamentos.component.css',
 })
