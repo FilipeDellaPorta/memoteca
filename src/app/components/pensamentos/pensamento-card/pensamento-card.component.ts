1f3b070c2c9f925ce5d331f21a1e5a5ecefe1f60
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-03-13 11:43:36 -0300

    fix: :bug: alterado o id para tipo string devido a versão do angular e importado o módulo router

--- src/app/components/pensamentos/pensamento-card/pensamento-card.component.ts
+++ src/app/components/pensamentos/pensamento-card/pensamento-card.component.ts
@@ -1,17 +1,18 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Pensamento } from '../Pensamento.js';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pensamento-card',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, RouterModule],
   templateUrl: './pensamento-card.component.html',
   styleUrl: './pensamento-card.component.css',
 })
 export class PensamentoCardComponent {
   @Input() pensamento: Pensamento = {
-    id: 0,
+    id: '0',
     conteudo: 'I love Angular',
     autoria: 'Nay',
     modelo: 'modelo3',
